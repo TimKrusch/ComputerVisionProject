a46f7b408fa1ed57af85c43bf55730369da53a44
TimKr <tim.krusch@web.de>
2018-06-22 17:42:20 +0200

    farbwertspreizung

--- identification2.py
+++ identification2.py
@@ -5,6 +5,8 @@ from PIL import Image, ImageTk, ImageEnhance
 import os
 import numpy as np
 
+from sklearn.cluster import MiniBatchKMeans
+
 
 def identify2(imagelist, folder_path):
     for image in imagelist:
@@ -20,14 +22,31 @@ def identify2(imagelist, folder_path):
         image_s = image_hsv[:,:,1]
 
         image_s_blur = cv2.medianBlur(image_s,9)
+        img_blur = cv2.medianBlur(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY),9)
 
 
-        circles = cv2.HoughCircles(image_s_blur,cv2.HOUGH_GRADIENT,1,40,
-                            param1=100,param2=40,minRadius=4,maxRadius=75)
+        img_color_spread = img
+
+        img_color_spread[:,:,0] = cv2.equalizeHist(img[:,:,0])
+        img_color_spread[:,:,1] = cv2.equalizeHist(img[:,:,1]) 
+        img_color_spread[:,:,2] = cv2.equalizeHist(img[:,:,2])
+
+        image_color_spread_hsv = cv2.cvtColor(img_color_spread, cv2.COLOR_BGR2HSV)
+
+        image_color_spread_s = image_color_spread_hsv[:,:,1]
+
+        image_color_spread_s_blur = cv2.medianBlur(image_color_spread_s,9)
+
+
+
+
+
+        circles = cv2.HoughCircles(image_color_spread_s_blur,cv2.HOUGH_GRADIENT,1,40,
+                            param1=100,param2=40,minRadius=5,maxRadius=75)
 
         #print(circles)
 
-        if circles is not None:
+        if circles is not None: 
         
             #print(circles)
             circles = np.uint64(np.around(circles))
@@ -39,15 +58,17 @@ def identify2(imagelist, folder_path):
                 cv2.circle(img,(i[0],i[1]),2,(0,0,255),3)
 
         
-        img=temp_match('achtung.jpg',(11,11,255),img,image_s,0.7)
-        img=temp_match('achtung2.jpg',(11,11,255),img,image_s,0.73)
-            #img=temp_match('gebot.jpg',(255,11,11),img,image_s,0.875)
-        img=temp_match('stop.jpg',(255,11,255),img,image_s,0.85)
-            #img=temp_match('haupt.jpg',(11,255,255),img,image_s,0.8)
-        img=temp_match('Vf_g.jpg',(11,255,11),img,image_s,0.7)
+        #img=temp_match('achtung.jpg',(11,11,255),img,image_s,0.7)
+        #img=temp_match('achtung2.jpg',(11,11,255),img,image_s,0.73)
+        #    #img=temp_match('gebot.jpg',(255,11,11),img,image_s,0.875)
+        #img=temp_match('stop.jpg',(255,11,255),img,image_s,0.85)
+        #    #img=temp_match('haupt.jpg',(11,255,255),img,image_s,0.8)
+        #img=temp_match('Vf_g.jpg',(11,255,11),img,image_s,0.7)
+        
 
+        
         cv2.imwrite(
-            folder_path + "/" + image.data["Name"][:-4] + "-edit.png", image_s
+            folder_path + "/" + image.data["Name"][:-4] + "-edit.png", img
         )
         image.data["Edit Img"] = folder_path + "/" + \
             image.data["Name"][:-4] + "-edit.png"
@@ -82,7 +103,7 @@ def temp_match(schild,farbe,img,img_s,thres):
         cv2.rectangle(img, pt, (pt[0] + 50, pt[1] + 50), farbe, 2)
 
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(template_matched)
-    print(max_val)
+    #print(max_val)
 
     #if max_val>0.700:
     #    top_left = max_loc
